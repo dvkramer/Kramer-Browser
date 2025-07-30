487935f1bda5d7ef2609adc251d376b6b71c28d4
Derpy153 <derpy153@gmail.com>
2025-07-29 23:33:44 -0400

    electron

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Kramer Browser
 
-Kramer Browser is a lightweight browser built on Electron.
+Kramer Browser is a WIP browser built on Electron.
 
 <img width="512" height="512" alt="github kbrowser" src="https://github.com/user-attachments/assets/c58b8741-dbd3-4bc5-83f9-935dc25df236" />
+
+## To Run
+
+1. Install dependencies:
+   ```
+   npm install
+   ```
+2. Start the application:
+   ```
+   npm start
+   ```
--- preload.js
+++ preload.js
@@ -1,21 +1,12 @@
-const { contextBridge, ipcRenderer } = require('electron');
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
 
-// Expose protected methods that allow the renderer process to use
-// the ipcRenderer without exposing the entire object
-contextBridge.exposeInMainWorld('electronAPI', {
-  // Menu actions
-  onMenuNewTab: (callback) => ipcRenderer.on('menu-new-tab', callback),
-  onMenuCloseTab: (callback) => ipcRenderer.on('menu-close-tab', callback),
-  onMenuGoBack: (callback) => ipcRenderer.on('menu-go-back', callback),
-  onMenuGoForward: (callback) => ipcRenderer.on('menu-go-forward', callback),
-  onMenuRefresh: (callback) => ipcRenderer.on('menu-refresh', callback),
-  onMenuGoHome: (callback) => ipcRenderer.on('menu-go-home', callback),
-  onMenuAbout: (callback) => ipcRenderer.on('menu-about', callback),
-
-  // App info
-  getVersion: () => ipcRenderer.invoke('get-version'),
-  showAbout: () => ipcRenderer.invoke('show-about'),
-
-  // Remove listeners
-  removeAllListeners: (channel) => ipcRenderer.removeAllListeners(channel)
-});
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+})
